bdeacbad9197702baa03ed5cdbcf13a0c2a644ae
samkimuel <samkim2626@gmail.com>
2022-05-30 21:38:12 +0900

    initial commit

---
+++ src/main/java/com/mhk/studyspringbatch/StudySpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.mhk.studyspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhk/studyspringbatch/StudySpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhk.studyspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
